cb84f0e76d944ecac908c5bcef70a4c6e3fc8047
Tanveer <tanveeratworks@gmail.com>
2025-06-15 13:23:31 +0530

    Initial commit

---
+++ app/src/main/java/com/devindepth/foodhub_android/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.devindepth.foodhub_android
+
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.devindepth.foodhub_android.ui.theme.FoodHubAndroidTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    var showSplashScreen = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                showSplashScreen
+            }
+            setOnExitAnimationListener { splashScreenView ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    splashScreenView.iconView,
+                    View.SCALE_X,
+                    0.5f,
+                    0f
+                )
+                val zoomY = ObjectAnimator.ofFloat(
+                    splashScreenView.iconView,
+                    View.SCALE_Y,
+                    0.5f,
+                    0f
+                )
+                zoomX.duration = 500
+                zoomY.duration = 500
+                zoomX.interpolator = OvershootInterpolator()
+                zoomY.interpolator = OvershootInterpolator()
+                zoomX.doOnEnd {
+                    splashScreenView.remove()
+                }
+                zoomY.doOnEnd {
+                    splashScreenView.remove()
+                }
+                zoomX.start()
+                zoomY.start()
+            }
+        }
+
+        enableEdgeToEdge()
+        setContent {
+            FoodHubAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(3000)
+            showSplashScreen = false
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FoodHubAndroidTheme {
+        Greeting("Android")
+    }
+}
